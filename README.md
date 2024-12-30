90681a46d2147b77533182607a4e34b14ad637ad
Elliot Jackson <elliotjackson@mailbox.org>
2024-12-30 13:13:29 +0000

    updates

---
+++ README.md
@@ -0,0 +1,12 @@
+# Dotfiles
+
+Managed by homeshick: https://github.com/andsens/homeshick
+
+## Setup
+
+1. Give Terminal full disk access
+2. `git clone https://github.com/elliotekj/dotfiles.git`
+3. Run `install/macos/install.sh`
+
+## Post-Setup
+
---
+++ home/install/macos/install.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+set -eu
+
+if [ "$(id -u)" -eq 0 ]; then
+  echo "The script is running as root, please run as a the user."
+  exit 1
+fi
+
+cd "$HOME"
+
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_TOOLCHAINS_HOME="$HOME/.local/toolchains"
+
+# -----------------------------------------------------------------------------
+# Xcode Command Line Tools if needed
+# -----------------------------------------------------------------------------
+
+if [ ! -e "/Library/Developer/CommandLineTools/usr/bin/git" ]; then
+  echo "Installing Xcode Command Line Tools"
+  # This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
+  clt_placeholder="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+  sudo touch "$clt_placeholder"
+  clt_label="$(/usr/sbin/softwareupdate -l | grep -B 1 -E 'Command Line Tools' | awk -F'*' '/^ *\*/ {print $2}' | sed -e 's/^ *Label: //' -e 's/^ *//' | sort -V | tail -n1)"
+  if [[ -n $clt_label ]]; then
+    echo "Installing $clt_label"
+    sudo "/usr/sbin/softwareupdate" "-i" "$clt_label"
+    sudo "/usr/bin/xcode-select" "--switch" "/Library/Developer/CommandLineTools"
+  fi
+  sudo rm -f "$clt_placeholder"
+fi
+
+# -----------------------------------------------------------------------------
+# Homebrew
+# -----------------------------------------------------------------------------
+
+brew=/opt/homebrew/bin/brew
+
+if [ ! -x $brew ]; then
+  echo "Install Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+$brew update
+$brew bundle --file=$HOME/install/macos/Brewfile
+$brew upgrade
+
+export PATH="$brew":$PATH
+
+# -----------------------------------------------------------------------------
+# Rust
+# -----------------------------------------------------------------------------
+
+export CARGO_HOME="$XDG_TOOLCHAINS_HOME/rust/cargo"
+export RUSTUP_HOME="$XDG_TOOLCHAINS_HOME/rust/rustup"
+mkdir -p "$CARGO_HOME"
+mkdir -p "$RUSTUP_HOME"
+if ! command -v rustup &>/dev/null; then
+  curl -fsSL https://sh.rustup.rs | /bin/sh -s -- -y --no-modify-path
+fi
+"$CARGO_HOME"/bin/rustup default stable
+"$CARGO_HOME"/bin/rustup component add rust-src rustfmt clippy
+
+# -----------------------------------------------------------------------------
+# ASDF
+# -----------------------------------------------------------------------------
+
+if [ ! -d "$HOME/.asdf" ]; then
+  git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.15.0
+  . "$HOME/.asdf/asdf.sh"
+  asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
+  export KERL_CONFIGURE_OPTIONS="--without-javac --with-ssl=$(brew --prefix openssl@1.1)"
+  asdf install erlang latest
+  asdf global erlang latest
+  asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
+  asdf install elixir latest
+  asdf global elixir latest
+  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  asdf install nodejs latest
+  asdf global nodejs latest
+  asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+  asdf install ruby latest
+  asdf global ruby latest
+  asdf plugin add python https://github.com/asdf-community/asdf-python 
+  asdf install python 3.12.2
+  asdf install python 2.7.18
+  asdf global python 3.12.2 2.7.18
+fi
+
+# -----------------------------------------------------------------------------
+# Config
+# -----------------------------------------------------------------------------
+
+chflags nohidden "$HOME/Library"
+if [[ -d "$HOME/Applications" ]]; then
+  chflags hidden "$HOME/Applications"
+fi
+if [[ -d "$HOME/Public" ]]; then
+  chflags hidden "$HOME/Public"
+fi
+
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain AppleInterfaceStyle "Dark"
+
+defaults write com.apple.dock show-recents -int 0
+defaults write com.apple.dock autohide -int 1
+defaults write com.apple.dock mru-spaces -bool false
+defaults write com.apple.dock magnification -int 0
+defaults write com.apple.dock mineffect "scale"
+defaults write com.apple.dock minimize-to-application -int 1
+
+defaults write com.apple.textedit RichText -int 0
+
+# Remap Caps Lock as Control (see https://developer.apple.com/library/archive/technotes/tn2450/_index.html)
+hidutil property --set '{
+  "UserKeyMapping": [{
+    "HIDKeyboardModifierMappingSrc": 0x700000039,
+    "HIDKeyboardModifierMappingDst": 0x7000000E0,
+  }]
+}'
+
+# Make sure that Spotify doesn't start on login...
+mkdir -p "$HOME/Library/Application\ Support/Spotify"
+touch "$HOME/Library/Application\ Support/Spotify/prefs"
+if ! grep -q "app.autostart-mode" "$HOME/Library/Application\ Support/Spotify/prefs"; then
+  echo 'app.autostart-mode="off"' >>"$HOME/Library/Application\ Support/Spotify/prefs"
+  echo 'app.autostart-banner-seen=true' >>"$HOME/Library/Application\ Support/Spotify/prefs"
+fi
+
+killall Finder
+killall Dock
+
+printf "\e[1;32m==> Done! You should now reboot.\n\e[0m"
+
