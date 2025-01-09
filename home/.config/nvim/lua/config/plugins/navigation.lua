63893977cbbd3641f6ea67fb3b1efc4d2b0f37d8
Elliot Jackson <elliot@elliotekj.com>
2025-01-09 09:26:19 +0000

    cleanup navigation config

--- home/.config/nvim/lua/config/plugins/navigation.lua
+++ home/.config/nvim/lua/config/plugins/navigation.lua
@@ -1,70 +1,11 @@
 return {
   {
-    'mhinz/vim-grepper',
-    config = function()
-      vim.g.grepper = {
-        tools = { 'rg', 'git', 'grep' },
-        highlight = 1,
-        rg = {
-          -- TODO: Remove --threads=3 once running on macos sequoia isn't so slow.. See https://github.com/BurntSushi/ripgrep/issues/2925
-          grepprg = 'rg -H --no-heading --vimgrep --threads=3',
-        },
-      }
-
-      vim.keymap.set('n', '<leader>/', ':Grepper -tool rg<CR>', { noremap = true, silent = true })
-      vim.keymap.set('n', 'gs', '<plug>(GrepperOperator)', { remap = true })
-      vim.keymap.set('x', 'gs', '<plug>(GrepperOperator)', { remap = true })
-    end,
-  },
-  {
     'MagicDuck/grug-far.nvim',
     config = function()
       require('grug-far').setup();
-      vim.keymap.set('n', 'gS', '<cmd>GrugFar<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', 'gpS', '<cmd>GrugFar<CR>', { noremap = true, silent = true })
     end
   },
-  {
-    'ibhagwan/fzf-lua',
-    config = function()
-      local fzf = require('fzf-lua')
-      local actions = require('fzf-lua.actions')
-      fzf.setup({
-        'max-perf',
-        winopts = {
-          backdrop = false,
-          border = 'single',
-          preview = {
-            hidden = 'hidden',
-          },
-        },
-        winopts_fn = function()
-          local split = 'belowright new'
-          local height = math.floor(vim.o.lines * 0.35)
-          return { split = split .. ' | resize ' .. tostring(height) }
-        end,
-        files = {
-          no_header = true,
-          no_header_i = true,
-          multiprocess = true,
-          git_icons = false,
-          file_icons = false,
-          color_icons = false,
-          cwd_prompt = false,
-          actions = {
-            ['ctrl-g'] = { actions.toggle_ignore },
-          },
-          fd_opts = [[--type file --color=never --hidden --follow --strip-cwd-prefix --exclude .git]],
-        },
-        fzf_opts = {
-          ['--ansi'] = true,
-          ['--layout'] = 'default',
-          ['--border'] = 'none',
-        },
-      })
-      -- vim.keymap.set('n', '<leader><leader>', fzf.files, { silent = true })
-    end,
-  },
-
   {
     'nvim-telescope/telescope.nvim',
     version = '*',
@@ -73,15 +14,21 @@ return {
       local builtin = require('telescope.builtin')
       local actions = require('telescope.actions')
 
-      vim.keymap.set('n', '<leader>;', builtin.buffers)
+      -- Project mappings
       vim.keymap.set('n', '<leader><leader>', builtin.find_files)
+      vim.keymap.set('n', '<leader>/', builtin.live_grep)
+      vim.keymap.set('n', 'gps', builtin.lsp_workspace_symbols)
+
+      -- Buffer mappings
       vim.keymap.set('n', '//', builtin.current_buffer_fuzzy_find)
+      vim.keymap.set('n', 'gbs', builtin.lsp_document_symbols)
+      vim.keymap.set('n', 'gbt', builtin.treesitter)
 
       telescope.setup({
         defaults = {
           mappings = {
             i = {
-              ['<esc>'] = actions.close, -- Close when hitting escape in insert mode, rather than going to normal mode
+              ['<esc>'] = actions.close,
               ['<C-q>'] = actions.smart_send_to_qflist + actions.open_qflist,
             },
             n = {
@@ -90,62 +37,13 @@ return {
           },
         },
         pickers = {
-          buffers = {
-            theme = 'ivy',
-            previewer = false,
-            results_title = false,
-            preview_title = false,
-            sorting_strategy = 'descending',
-            sort_lastused = true,
-            sort_mru = true,
-            layout_config = {
-              height = 0.35,
-              prompt_position = 'bottom',
-              preview_width = 0.45,
-            },
-            mappings = {
-              i = {
-                ['<C-d>'] = actions.delete_buffer,
-              },
-              n = {
-                ['<C-d>'] = actions.delete_buffer,
-              },
-            },
-          },
           find_files = {
-            find_command = {
-              'fd',
-              '--type',
-              'file',
-              '--color=never',
-              '--hidden',
-              '--strip-cwd-prefix',
-              '--exclude',
-              '.git',
-            },
-            theme = 'ivy',
-            results_title = false,
-            preview_title = false,
-            sorting_strategy = 'descending',
-            layout_config = {
-              height = 0.35,
-              prompt_position = 'bottom',
-              preview_width = 0.45,
-            },
+            find_command = { "fd", "--type", "file", "--hidden", "--follow", "--strip-cwd-prefix", "--exclude", ".git" },
           },
-          git_files = {
-            theme = 'ivy',
-            results_title = false,
-            preview_title = false,
-            sorting_strategy = 'descending',
-            layout_config = {
-              height = 0.35,
-              prompt_position = 'bottom',
-              preview_width = 0.45,
-            },
-          },
-          current_buffer_fuzzy_find = {
-            sorting_strategy = 'ascending',
+          live_grep = {
+            additional_args = function()
+              return { "--hidden", "--glob", "!.git/*" }
+            end,
           },
         },
         extensions = {
@@ -159,7 +57,6 @@ return {
       })
 
       telescope.load_extension('fzf')
-      telescope.load_extension('vim_bookmarks')
     end,
     dependencies = {
       'nvim-lua/popup.nvim',
@@ -189,83 +86,37 @@ return {
   {
     'stevearc/oil.nvim',
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    config = true
+    config = function()
+      require("oil").setup()
+      vim.keymap.set("n", "<leader>fe", ":Oil<CR>", { noremap = true, silent = true })
+    end,
   },
   {
-    "rgroli/other.nvim",
+    'tpope/vim-projectionist',
     config = function()
-      require("other-nvim").setup({
-        mappings = {
-          "rust",
-          -- elixir & phoenix
-          {
-            pattern = "lib/(.*)_web/live/(.*)_live/(.*).html.heex",
-            target = { { target = "lib/%1_web/live/%2_live/%3.ex" } },
-          },
-          {
-            pattern = "lib/(.*).ex",
-            target = { { target = "test/%1_test.exs" } },
-          },
-          {
-            pattern = "lib/(.*)/(.*).ex",
-            target = { { target = "test/%1/%2_test.exs" } },
-          },
-          {
-            pattern = "test/(.*)_test.exs",
-            target = { { target = "lib/%1.ex" } },
-          },
-        },
-      })
+      vim.keymap.set("n", "<leader>fa", ":A<CR>", { noremap = true, silent = true })
     end,
+  },
+  {
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble",
     keys = {
       {
-        "<leader>fa",
-        function()
-          vim.cmd("Other")
-        end,
-        desc = "Alternate",
+        "gpd",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
       },
       {
-        "<leader>fm",
-        function()
-          vim.cmd("Oil")
-        end,
-        desc = "Manager (Oil)",
-      },
-      {
-        "<leader>fA",
-        function()
-          vim.cmd("OtherVSplit")
-        end,
-        desc = "Alternate (VSplit)",
+        "gbd",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
       },
     },
   },
   {
-    'mattesgroeger/vim-bookmarks',
-    config = function()
-      vim.g.bookmark_save_per_working_dir = true
-      vim.g.bookmark_auto_save = true
-      vim.g.bookmark_display_annotation = true
-      vim.g.bookmark_show_toggle_warning = false
-
-      local bookmarks = require('telescope').extensions.vim_bookmarks
-
-      local common_settings = {
-        layout_strategy = 'vertical',
-      }
-
-      local function all()
-        bookmarks.all(common_settings)
-      end
-
-      local function current_file()
-        bookmarks.current_file(common_settings)
-      end
-
-      vim.keymap.set('n', 'mg', current_file)
-      vim.keymap.set('n', 'mG', all)
-    end,
-  },
-  'ddeville/telescope-vim-bookmarks.nvim',
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+  }
 }
