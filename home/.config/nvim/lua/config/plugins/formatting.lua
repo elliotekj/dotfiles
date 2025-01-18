f36061a5389ffef89136466e17b19660476f28c3
Elliot Jackson <elliot@elliotekj.com>
2025-01-18 09:21:38 +0000

    reconfigure conform

--- home/.config/nvim/lua/config/plugins/formatting.lua
+++ home/.config/nvim/lua/config/plugins/formatting.lua
@@ -1,50 +1,48 @@
 return {
   'stevearc/conform.nvim',
   config = function()
-    local conform = require('conform')
-
-    conform.formatters.shfmt = {
-      inherit = false,
-      command = 'shfmt',
-      args = { '-filename', '$FILENAME', '--simplify', '--indent', '2' },
-    }
-    conform.formatters.black = {
-      inherit = false,
-      command = 'black',
-      args = {
-        '--stdin-filename',
-        '$FILENAME',
-        '--quiet',
-        '--line-length',
-        '100',
-        '-',
-      },
-    }
-
-    conform.setup({
-      format_after_save = {
-        lsp_fallback = true,
-      },
-      -- Don't show formatter errors since this gets super annoying and takes
-      -- focus when saving with a syntax error.
-      notify_on_error = false,
+    require("conform").setup({
       formatters_by_ft = {
-        -- bzl = { 'buildifier' },
-        -- c = { 'clang_format' },
-        -- cpp = { 'clang_format' },
-        -- cuda = { 'clang_format' },
-        elixir = { 'mix' },
-        -- fish = { 'fish_indent' },
-        go = { 'gofmt' },
-        lua = { 'stylua' },
-        -- objc = { 'clang_format' },
-        -- objcpp = { 'clang_format' },
-        proto = { 'buf' },
-        python = { 'ruff', 'black', 'isort' },
-        rust = { 'rustfmt' },
-        sh = { 'shfmt' },
-        terraform = { 'terraform_fmt' },
+        lua = { "stylua" },
+        elixir = { "mix" },
+        python = { "isort", "black" },
+        rust = { "rustfmt", lsp_format = "fallback" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
       },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
     })
+
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+
+    vim.keymap.set('n', '<localleader>tf', function()
+      if vim.g.disable_autoformat then
+        vim.cmd('FormatEnable')
+      else
+        vim.cmd('FormatDisable')
+      end
+    end, { desc = 'Toggle format on save' })
   end,
 }
