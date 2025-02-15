613dfe8451a3e583396d04ccf778e78e6afbfc38
Elliot Jackson <elliot@elliotekj.com>
2025-02-15 14:22:52 +0000

    add Trim

--- home/.config/nvim/lua/config/plugins/formatting.lua
+++ home/.config/nvim/lua/config/plugins/formatting.lua
@@ -1,48 +1,56 @@
 return {
-  'stevearc/conform.nvim',
-  config = function()
-    require("conform").setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        elixir = { "mix" },
-        python = { "isort", "black" },
-        rust = { "rustfmt", lsp_format = "fallback" },
-        javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
-      format_on_save = function(bufnr)
-        -- Disable with a global or buffer-local variable
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-          return
+  {
+    'stevearc/conform.nvim',
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+          elixir = { "mix" },
+          python = { "isort", "black" },
+          rust = { "rustfmt", lsp_format = "fallback" },
+          javascript = { "prettierd", "prettier", stop_after_first = true },
+        },
+        format_on_save = function(bufnr)
+          -- Disable with a global or buffer-local variable
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 500, lsp_format = "fallback" }
+        end,
+      })
+
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
         end
-        return { timeout_ms = 500, lsp_format = "fallback" }
-      end,
-    })
+      end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+      })
 
-    vim.api.nvim_create_user_command("FormatDisable", function(args)
-      if args.bang then
-        -- FormatDisable! will disable formatting just for this buffer
-        vim.b.disable_autoformat = true
-      else
-        vim.g.disable_autoformat = true
-      end
-    end, {
-      desc = "Disable autoformat-on-save",
-      bang = true,
-    })
+      vim.api.nvim_create_user_command("FormatEnable", function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = "Re-enable autoformat-on-save",
+      })
 
-    vim.api.nvim_create_user_command("FormatEnable", function()
-      vim.b.disable_autoformat = false
-      vim.g.disable_autoformat = false
-    end, {
-      desc = "Re-enable autoformat-on-save",
-    })
-
-    vim.keymap.set('n', '<localleader>tf', function()
-      if vim.g.disable_autoformat then
-        vim.cmd('FormatEnable')
-      else
-        vim.cmd('FormatDisable')
-      end
-    end, { desc = 'Toggle format on save' })
-  end,
+      vim.keymap.set('n', '<localleader>tf', function()
+        if vim.g.disable_autoformat then
+          vim.cmd('FormatEnable')
+        else
+          vim.cmd('FormatDisable')
+        end
+      end, { desc = 'Toggle format on save' })
+    end,
+  },
+  {
+    "cappyzawa/trim.nvim",
+    opts = {
+      trim_on_write = false
+    }
+  }
 }
