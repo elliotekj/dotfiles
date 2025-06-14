ec17c191b168c37f297eea8c88d2ec8e2a11ccc4
Elliot Jackson <elliot@elliotekj.com>
2025-06-14 18:26:10 +0100

    add peek

--- home/.config/nvim/lua/config/plugins/notes.lua
+++ home/.config/nvim/lua/config/plugins/notes.lua
@@ -60,5 +60,17 @@ return {
             "ScratchSplit",
         },
         opts = {},
-    }
+    },
+    {
+        "toppair/peek.nvim",
+        event = { "VeryLazy" },
+        build = "deno task --quiet build:fast",
+        config = function()
+            require("peek").setup({
+                filetype = { 'markdown', 'conf' }
+            })
+            vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+            vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+        end,
+    },
 }
