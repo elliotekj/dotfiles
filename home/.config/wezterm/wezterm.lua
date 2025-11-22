d80b8e251f4993db2feb973ad68acb8e4dd06802
Elliot Jackson <elliot@elliotekj.com>
2025-11-22 11:29:29 +0000

    feat: hide tabbar if only 1 tab

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -9,6 +9,7 @@ config.font_size = 14.0
 config.line_height = 1.2
 
 config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = 'RESIZE'
 
 config.scrollback_lines = 5000
