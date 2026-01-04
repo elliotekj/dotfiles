7e81e89f9b6a3085d640c59f8ef9e32b04063e69
Elliot Jackson <elliot@elliotekj.com>
2026-01-04 00:19:20 +0000

    feat: add mutt and gmail config

---
+++ home/.mutt/offlineimap-helpers.py
@@ -0,0 +1,28 @@
+import subprocess
+
+def get_pass():
+    return subprocess.check_output(
+        ["op", "read", "op://42uuu6jy35zg6fldljj4xco6xm/s3mtpagvsspfqyac3cqmr3y6ey/password"],
+        text=True
+    ).strip()
+
+LOCAL_TO_REMOTE = {
+    'inbox':   'INBOX',
+    'sent':    '[Gmail]/Sent Mail',
+    'drafts':  '[Gmail]/Drafts',
+    'trash':   '[Gmail]/Trash',
+    'archive': '[Gmail]/All Mail',
+}
+
+REMOTE_TO_LOCAL = {v: k for k, v in LOCAL_TO_REMOTE.items()}
+
+REMOTE_FOLDERS = list(LOCAL_TO_REMOTE.values())
+
+def nametrans_local(folder):
+    return LOCAL_TO_REMOTE.get(folder, folder)
+
+def nametrans_remote(folder):
+    return REMOTE_TO_LOCAL.get(folder, folder)
+
+def folderfilter(folder):
+    return folder in REMOTE_FOLDERS
