66104f51c8281c6b089e2face4d1fa855754cc25
Elliot Jackson <elliot@elliotekj.com>
2026-04-20 09:05:20 +0100

    feat: pull submodules

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -1,73 +1,19 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    config = function()
-      local parser_install_dir = vim.fn.stdpath('data') .. '/treesitter-parsers'
-      vim.opt.runtimepath:append(parser_install_dir)
-
-      require('nvim-treesitter.configs').setup({
-        parser_install_dir = parser_install_dir,
-        ensure_installed = {
-          'awk',
-          'bash',
-          'c',
-          'cmake',
-          'comment',
-          'cpp',
-          'css',
-          'diff',
-          'dockerfile',
-          'eex',
-          'elixir',
-          'erlang',
-          'git_config',
-          'git_rebase',
-          'gitattributes',
-          'gitignore',
-          'go',
-          'gomod',
-          'gosum',
-          'gowork',
-          'heex',
-          'html',
-          'javascript',
-          'json',
-          'jq',
-          'lua',
-          'luadoc',
-          'make',
-          'markdown',
-          'ninja',
-          'objc',
-          'proto',
-          'python',
-          'regex',
-          'rust',
-          'sql',
-          'terraform',
-          'toml',
-          'typescript',
-          'vim',
-          'vimdoc',
-          'yaml',
-        },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "e",
-            node_incremental = "e",
-            node_decremental = "E",
-          },
-        },
-      })
-    end,
+    lazy = false,
     build = ':TSUpdate',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-  },
+    config = function ()
+      require('nvim-treesitter').setup {
+        install_dir = vim.fn.stdpath('data') .. '/site'
+      }
+
+      require('nvim-treesitter').install { 'elixir', 'erlang' }
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'elixir', 'erlang' },
+        callback = function() vim.treesitter.start() end,
+      })
+    end
+  }
 }
